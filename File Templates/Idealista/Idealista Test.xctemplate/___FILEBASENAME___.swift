96202615b8f4979734d63661c1015229e47e6fdd
Miguel Olmedo <molmedo@idealista.com>
2015-09-17 12:16:24 +0200

    Initial templates

---
+++ File Templates/Idealista/Idealista Test.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,24 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import XCTest
+
+class ___FILEBASENAMEASIDENTIFIER___: XCTest
+{
+    
+    override func setUp()
+    {
+        super.setUp()
+    }
+    
+    override func tearDown()
+    {
+    
+        super.tearDown()
+    }
+}
